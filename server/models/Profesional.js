3e4a286f557e72db23d7b5e038b7f99738de5ad3
Jorge <jlegazpegutierrez@gmail.com>
2019-02-27 18:48:21 +0100

    Login y signup OK.  local strategy

---
+++ server/models/Profesional.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const proSchema = new Schema(
+  {
+    empresa: String,
+    name: String,
+    email: String,
+    password: String,
+    phone: Number,
+    ubication: String,
+    rol: [{ type: String, enum: ["user", "profesional"] }],
+    typePro: [
+      {
+        type: String,
+        enum: [
+          "cristalero",
+          "fontanero",
+          "electricista",
+          "carpintero",
+          "albañil"
+        ]
+      }
+    ]
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const Pro = mongoose.model("Profesional", proSchema);
+module.exports = Pro;
