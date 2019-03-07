0cc7c78e9c6db4dd9bb64e0bcc157d30288eb7e0
Jorge <jlegazpegutierrez@gmail.com>
2019-03-07 13:13:45 +0100

    Rutas works

--- server/routes/works.js
+++ server/routes/works.js
@@ -13,4 +13,37 @@ works.get("/profesionales", (req, res, next) => {
     });
 });
 
+works.post("/profesionales", (req, res, next) => {
+  Relation.create({
+    user: req.body.idUser,
+    profesional: req.body.idPro
+  })
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+works.get("/perfilpro", (req, res, next) => {
+  Relation.find()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+works.get("/perfiluser", (req, res, next) => {
+  Relation.find()
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 module.exports = works;
